d42e3aec30ef6c26ac89bb71952395da3f802d24
rbrt-s <robert.seibert@inovex.de>
2019-07-02 20:36:53 +0200

    next step testing

--- action-addShoppingItem.js
+++ action-addShoppingItem.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
 const { withHermes } = require('hermes-javascript')
 const Intents = require('./lib/intents');
+const Todoist = require('./lib/todoist');
 
 withHermes(hermes => {
+    const todoist = await Todoist.factory();
     const dialog = hermes.dialog()
 
     dialog.flow(Intents.addShoppingItem, (msg, flow) => {
-        const item = msg.slots.find(slot => slot.slotName == 'item').rawValue;
+        const item = msg.slots.find(slot => slot.slotName == 'item');
+        const content = item.rawValue;
+        const confidence = item.confidenceScore;
+
+        const commands = [{
+            type: Todoist.Commands.item_add,
+            uuid:  uuidv4(),
+            temp_id: uuidv4(),
+            args: {
+                content: content,
+                project_id: todoist.projectId
+            }
+        }];
+
+        await todoist.write(commands);
         flow.end();
-        return `<speak> <s>Ich habe <emphasis level=\"moderate\">${item}</emphasis> der Einkaufsliste hinzugefügt.</s></speak>`;
+        
+        return `<speak>
+            <s>Ich habe <emphasis level=\"moderate\">${content}</emphasis> der Einkaufsliste hinzugefügt.</s>
+            <s>Der Erkennungswert war ${confidence}</s>
+        </speak>`;
     });
 });
